bdeff3aa3db31e6fd4262524029028c074363cc5
Ray Scherrer <rwscherrer@gmail.com>
2016-08-27 18:36:33 -0500

    update

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -1,7 +1,16 @@
 # Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
 
 def find_longest_word(sentence)
-
+	sub_s = sentence.chop.split(" ")
+	count = 0
+	longest = sub_s[count]
+	sub_s.each do |word|
+		if sub_s[count].length > longest.length
+			longest = sub_s[count]
+		end
+		count += 1
+	end
+	result = longest
 end
 
 # Driver code - don't touch anything below this line.
--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -3,7 +3,7 @@
 # comes with Ruby!
 
 def reverse_a_string(string)
-
+  string = string.reverse!
 end
 
 # Driver code - don't touch anything below this line.
